b028f4b375e9ea5a84b25623e9c98030b0ec9765
Simon Racine-Chevalier <simonrchevalier@gmail.com>
2025-03-20 16:00:02 -0600

    feat: add URLs for LinkedIn profile and PDF resume

---
+++ src/lib/urls.ts
@@ -0,0 +1,3 @@
+export const linkedInProfileUrl = "https://www.linkedin.com/in/simonrchevalier";
+export const pdfResumeUrl =
+  "https://www.icloud.com/iclouddrive/070jPiH_ZITYIVH6Wm6sa946g#cv-simonrchevalier";
