3cce6d3da2eccda58bfc5cc2f4b0aa96c501d8e7
Simon Racine-Chevalier <simonrchevalier@gmail.com>
2025-03-20 16:11:10 -0600

    feat: update portfolio items and add new project pages for Busbud, Bopper, Gaia, Horizon, and Vida Verde

---
+++ src/routes/work/bopper-music-licensing/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { portfolioItems } from "$lib/portfolio-items";
+  const {
+    bopper: { altText, imageUrl, title },
+  } = portfolioItems;
+</script>
+
+<main>
+  <article class="px-container container-grid">
+    <header class="col-span-10 col-start-2 mb-8">
+      <h2 class="text-fluid-xl mb-2 font-bold tracking-tight text-balance">
+        {title}
+      </h2>
+      <h3 class="text-fluid-md font-bold opacity-50">Coming soon</h3>
+    </header>
+    <picture class="aspect-golden relative col-span-10 col-start-2 overflow-hidden rounded-lg">
+      {#each imageUrl.slice(1) as srcset, i}
+        <source {srcset} />
+      {/each}
+      <img src={imageUrl[0]} alt={altText} class="absolute object-cover" />
+    </picture>
+  </article>
+</main>
